39c423e8d68d148e26431343901fe08dc0e18d4b
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-02 11:07:23 -0500

    Streamline Runner.start

--- lib/aoc2021/runner.rb
+++ lib/aoc2021/runner.rb
@@ -6,22 +6,9 @@ module AoC2021
   # The Runner class provides file loading services around the solution for each day.
   class Runner
     def self.start
-      # Read and parse the input file for day 1.
-      # Invoke Day01 with the input array.
-      # Output the results.
-      File.open("input/day01a.txt") do |file|
-        print "Day 1, part A: "
-        puts " #{SonarDepth.new(
-          file.readlines.map(&:to_i)
-        ).count_increases } increases"
-      end
-
-      File.open("input/day01a.txt") do |file|
-        print "Day 1, part B: "
-        puts " #{SonarDepth.new(
-          file.readlines.map(&:to_i)
-        ).count_triplet_increases } triplet increases"
-      end
+      depths = File.open("input/day01a.txt") { |file| SonarDepth.new(file.readlines.map(&:to_i)) }
+      puts "Day 1, part A: #{depths.count_increases} increases"
+      puts "Day 1, part B: #{depths.count_triplet_increases} triplet increases"
     end
   end
 end
