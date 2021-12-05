20001353e7433905c84d8442666ada8732f81660
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-05 18:28:09 -0500

    Fix some reeks in the Vents class

--- lib/aoc2021/vents.rb
+++ lib/aoc2021/vents.rb
@@ -2,14 +2,34 @@
 
 require "forwardable"
 
+# Point class to handle X and y coordinates
+class Point
+  attr_reader :x, :y
+
+  # noinspection RubyInstanceVariableNamingConvention
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def offset_and_slope(other)
+    other_y = other.y
+    if @x < other.x
+      [@y, @y < other_y ? 1 : -1]
+    else
+      [other_y, other_y < @y ? 1 : -1]
+    end
+  end
+end
+
 module AoC2021
   # Calculates conditions of victory for given plays and boards
   class Vents
     def initialize(file)
       @board = []
       @lines = file.readlines(chomp: true).map { |line| line.split(/\D/).reject(&:empty?).map(&:to_i) }
-      parse_horizontal { |line| line[1] == line[3] }
-      parse_vertical { |line| line[0] == line[2] && line[1] != line[3] }
+      parse_horizontal { |_, y_one, _, y_two| y_one == y_two }
+      parse_vertical { |x_one, y_one, x_two, y_two| x_one == x_two && y_one != y_two }
     end
 
     def line_loop(filter_criteria, &block)
@@ -17,29 +37,26 @@ module AoC2021
     end
 
     def parse_horizontal(&filter_criteria)
-      line_loop(filter_criteria) do |line|
-        ([line[0], line[2]].min..[line[0], line[2]].max).each do |x_index|
-          y_index = line[1]
+      line_loop(filter_criteria) do |x_one, y_index, x_two, _|
+        ([x_one, x_two].min..[x_one, x_two].max).each do |x_index|
           count_vent x_index, y_index
         end
       end
     end
 
     def parse_vertical(&filter_criteria)
-      line_loop(filter_criteria) do |line|
-        ([line[1], line[3]].min..[line[1], line[3]].max).each do |y_index|
-          x_index = line[0]
+      line_loop(filter_criteria) do |x_index, y_one, _, y_two|
+        ([y_one, y_two].min..[y_one, y_two].max).each do |y_index|
           count_vent x_index, y_index
         end
       end
     end
 
     def parse_diagonal(&filter_criteria)
-      line_loop(filter_criteria) do |line|
-        offset, slope = offset_and_slope(line)
-        ([line[0], line[2]].min..[line[0], line[2]].max).each_with_index do |x_index, idx|
-          y_index = offset + (slope * idx)
-          count_vent x_index, y_index
+      line_loop(filter_criteria) do |x_one, y_one, x_two, y_two|
+        offset, slope = Point.new(x_one, y_one).offset_and_slope(Point.new(x_two, y_two))
+        ([x_one, x_two].min..[x_one, x_two].max).each_with_index do |x_index, idx|
+          count_vent x_index, offset + (slope * idx)
         end
       end
     end
@@ -49,23 +66,16 @@ module AoC2021
     end
 
     def overlaps_with_diagonals
-      parse_diagonal { |line| line[0] != line[2] && line[1] != line[3] }
+      parse_diagonal { |x_one, y_one, x_two, y_two| x_one != x_two && y_one != y_two }
       overlaps
     end
 
     private
 
     def count_vent(x_index, y_index)
-      @board[y_index]          = [] unless @board[y_index]
-      @board[y_index][x_index] = 1 + (@board[y_index][x_index] || 0)
-    end
-
-    def offset_and_slope(line)
-      if line[0] < line[2]
-        [line[1], line[1] < line[3] ? 1 : -1]
-      else
-        [line[3], line[3] < line[1] ? 1 : -1]
-      end
+      row             = @board[y_index] || []
+      row[x_index]    = (row[x_index] || 0) + 1
+      @board[y_index] = row
     end
   end
 end
