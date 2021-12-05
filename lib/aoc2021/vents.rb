2d7cdb63cd2cd82b1d7da15ba0e24c34f7bb0af9
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-05 13:54:40 -0500

    Satisfy Rubocop

--- lib/aoc2021/vents.rb
+++ lib/aoc2021/vents.rb
@@ -8,50 +8,38 @@ module AoC2021
     def initialize(file)
       @board = []
       @lines = file.readlines(chomp: true).map { |line| line.split(/\D/).reject(&:empty?).map(&:to_i) }
-      # @lines.each do |line|
-      #   pp line
-      # end
-      parse_horizontal
-      parse_vertical
+      parse_horizontal { |line| line[1] == line[3] }
+      parse_vertical { |line| line[0] == line[2] && line[1] != line[3] }
     end
 
-    def parse_horizontal
-      @lines.filter { |line| line[1] == line[3] }.each do |line|
+    def line_loop(filter_criteria, &block)
+      @lines.filter(&filter_criteria).each(&block)
+    end
+
+    def parse_horizontal(&filter_criteria)
+      line_loop(filter_criteria) do |line|
         ([line[0], line[2]].min..[line[0], line[2]].max).each do |x_index|
-          @board[line[1]]          = [] unless @board[line[1]]
-          @board[line[1]][x_index] = 1 + (@board[line[1]][x_index] || 0)
+          y_index = line[1]
+          count_vent x_index, y_index
         end
       end
     end
 
-    def parse_vertical
-      @lines.filter { |line| line[0] == line[2] && line[1] != line[3] }.each do |line|
+    def parse_vertical(&filter_criteria)
+      line_loop(filter_criteria) do |line|
         ([line[1], line[3]].min..[line[1], line[3]].max).each do |y_index|
-          @board[y_index]          = [] unless @board[y_index]
-          @board[y_index][line[0]] = 1 + (@board[y_index][line[0]] || 0)
+          x_index = line[0]
+          count_vent x_index, y_index
         end
       end
     end
 
-    def offset_and_slope(line)
-      # 6,4 -> 2, 0  == [2, 0], [3, 1], [4, 2], [5, 3], [6, 4]
-      if line[0] < line[2]
-        [line[1], line[1] < line[3] ? 1 : -1]
-      else
-        [line[3], line[3] < line[1] ? 1 : -1]
-      end
-      #  [8, -1]
-    end
-
-    def parse_diagonal
-      @lines.filter { |line| line[0] != line[2] && line[1] != line[3] }.each do |line|
+    def parse_diagonal(&filter_criteria)
+      line_loop(filter_criteria) do |line|
         offset, slope = offset_and_slope(line)
         ([line[0], line[2]].min..[line[0], line[2]].max).each_with_index do |x_index, idx|
-          # pp x_index, offset, slope, idx
           y_index = offset + (slope * idx)
-          # puts "Marking #{x_index}, #{y_index}"
-          @board[y_index] = [] unless @board[y_index]
-          @board[y_index][x_index] = 1 + (@board[y_index][x_index] || 0)
+          count_vent x_index, y_index
         end
       end
     end
@@ -61,9 +49,23 @@ module AoC2021
     end
 
     def overlaps_with_diagonals
-      parse_diagonal
-      # pp @board
+      parse_diagonal { |line| line[0] != line[2] && line[1] != line[3] }
       overlaps
+    end
+
+    private
+
+    def count_vent(x_index, y_index)
+      @board[y_index]          = [] unless @board[y_index]
+      @board[y_index][x_index] = 1 + (@board[y_index][x_index] || 0)
+    end
+
+    def offset_and_slope(line)
+      if line[0] < line[2]
+        [line[1], line[1] < line[3] ? 1 : -1]
+      else
+        [line[3], line[3] < line[1] ? 1 : -1]
+      end
     end
   end
 end
