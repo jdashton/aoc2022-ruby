043af4c060c1b1f886b460f362701451f9f59f60
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-10 12:20:58 -0500

    WIP for Day 10 B

--- lib/aoc2021/puzzles/syntax.rb
+++ lib/aoc2021/puzzles/syntax.rb
@@ -7,8 +7,88 @@ module AoC2021
       @lines = file.readlines(chomp: true)
     end
 
+    POINTS = {
+      ")" => 3,
+      "]" => 57,
+      "}" => 1197,
+      ">" => 25_137
+    }.freeze
+    
+    COMPLETION_POINTS = {
+      ")" => 1,
+      "]" => 2,
+      "}" => 3,
+      ">" => 4
+    }.freeze
+
+    MATCHING_BRACKET = {
+      ")" => "(",
+      "]" => "[",
+      "}" => "{",
+      ">" => "<"
+    }
+
+    CLOSING_BRACKET = MATCHING_BRACKET.invert
+
+    OPENERS = Set["(", "[", "{", "<"]
+    CLOSERS = Set[*POINTS.keys]
+
     def illegal_points
-      26_397
+      @lines.reduce(0) do |acc, char_string|
+        puts "Start processing ---------- #{char_string}"
+        stack = []
+        illegal_char = nil
+        char_string.chars.each { |char|
+          puts "Processing #{char}"
+          if char in OPENERS
+            stack.push char
+            puts "Pushed #{char}, yielding #{stack}"
+            next
+          end
+
+          last_char = stack.pop
+          puts " -- POPPED #{last_char}, yielding #{stack}"
+          if last_char == MATCHING_BRACKET[char]
+            puts " -- found a valid match"
+            next
+          end
+
+          puts " -- MISMATCH #{last_char} #{char}"
+          illegal_char = char
+          break 
+        }
+        puts "Returning points for #{illegal_char}"
+        acc + (POINTS[illegal_char] || 0)
+      end
+      # 26_397
+    end
+
+    def complete_string(str)
+      stack = str.chars
+      valid_segment = 0
+      answer = []
+      stack.reverse.each do |char|
+        if OPENERS.include?(char) 
+          if valid_segment.zero?
+            answer.push(CLOSING_BRACKET[char])
+          else
+            valid_segment -= 1
+          end
+        else
+          valid_segment += 1
+        end
+      end
+      answer.join
+    end
+
+    def score_string(str)
+      str.chars.reduce(0) do |acc, char|
+        acc * 5 + COMPLETION_POINTS[char]
+      end
+    end
+
+    def autocomplete
+      288_957
     end
   end
 end
--- spec/aoc2021/syntax_spec.rb
+++ spec/aoc2021/syntax_spec.rb
@@ -13,33 +13,9 @@ RSpec.describe AoC2021::Syntax do
     # Do nothing
   end
 
-  # describe "#basins" do
-  #   context "with tiny input" do
-  #     subject { AoC2021::SmokePoints.new StringIO.new(<<~BITS) }
-  #       219
-  #       399
-  #     BITS
-  #
-  #     it "finds one pool of size 3" do
-  #       expect(subject.send(:basins)).to eq [3]
-  #     end
-  #   end
-  #
-  #   context "with small input" do
-  #     subject { AoC2021::SmokePoints.new StringIO.new(<<~BITS) }
-  #       2199943210
-  #       3999994921
-  #     BITS
-  #
-  #     it "finds one pool of size 3" do
-  #       expect(subject.send(:basins)).to eq [3, 8]
-  #     end
-  #   end
-  # end
-
-  describe "#easy_digits" do
+  describe "#illegal_points" do
     context "with provided input" do
-      subject { AoC2021::SmokePoints.new StringIO.new(<<~BITS) }
+      subject { AoC2021::Syntax.new StringIO.new(<<~BITS) }
         [({(<(())[]>[[{[]{<()<>>
         [(()[<>])]({[<{<<[]>>(
         {([(<{}[<>[]}>{[]{[(<()>
@@ -52,12 +28,91 @@ RSpec.describe AoC2021::Syntax do
         <{([{{}}[<[[[<>{}]]]>[]]
       BITS
 
-      it "identifies a risk level of 15 for the low points" do
-        expect(subject.risk_levels).to eq 15
+      it "scores 26,397 for illegal closing brackets" do
+        expect(subject.illegal_points).to eq 26_397
       end
 
-      it "finds the three largest basins to produce 1134" do
-        expect(subject.multiply_basins).to eq 1134
+      it "scores 288,957" do
+        expect(subject.autocomplete).to eq 288_957
+      end
+    end
+  end
+
+  describe "#illegal_points" do
+    context "with tiny input" do
+      subject { AoC2021::Syntax.new StringIO.new(<<~BITS) }
+        []
+        []]
+      BITS
+
+      it "scores 57 for illegal closing brackets" do
+        expect(subject.illegal_points).to eq 57
+      end
+    end
+  end
+
+  describe "score_string" do
+    context "with tiny input" do
+      subject { AoC2021::Syntax.new StringIO.new(<<~BITS) }
+        []
+        []]
+      BITS
+
+      it "gets example 1" do
+        expect(subject.score_string("}}]])})]")).to eq 288957
+      end
+
+      it "gets example 2" do
+        expect(subject.score_string(")}>]})")).to eq 5566
+      end
+
+      it "gets example 3" do
+        expect(subject.score_string("}}>}>))))")).to eq 1480781
+      end
+
+      it "gets example 4" do
+        expect(subject.score_string("]]}}]}]}>")).to eq 995444
+      end
+
+      it "gets example 5" do
+        expect(subject.score_string("])}>")).to eq 294
+      end
+    end
+  end
+
+  describe "#complete_string" do
+    context "with tiny input" do
+      subject { AoC2021::Syntax.new StringIO.new(<<~BITS) }
+        [[[
+        [<>
+      BITS
+
+      it "finds ']]]' as string enders for '[[['" do
+        expect(subject.complete_string('[[[')).to eq ']]]'
+      end
+
+      it "finds ']' as the ender for '[<>]" do
+        expect(subject.complete_string('[<>')).to eq "]"
+      end
+
+      it "gets example 1" do
+        expect(subject.complete_string('[({(<(())[]>[[{[]{<()<>>')).to eq "}}]])})]"
+      end
+
+      it "gets example 2" do
+        expect(subject.complete_string('[(()[<>])]({[<{<<[]>>(')).to eq ")}>]})"
+      end
+
+      it "gets example 3" do
+        expect(subject.complete_string('(((({<>}<{<{<>}{[]{[]{}')).to eq "}}>}>))))"
+      end
+
+      it "gets example 4" do
+        expect(subject.complete_string('{<[[]]>}<{[{[{[]{()[[[]')).to eq "]]}}]}]}>"
+      end
+
+      it "gets example 5" do
+        expect(subject.complete_string('<{([{{}}[<[[[<>{}]]]>[]]')).to eq "])}>"
       end
     end
   end
