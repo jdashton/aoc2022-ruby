4609485e90664e24b53dbb6cefb0a325ab9cbcd6
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-17 10:57:19 -0500

    slight progress

--- lib/aoc2021/puzzles/trick_shot.rb
+++ lib/aoc2021/puzzles/trick_shot.rb
@@ -5,8 +5,33 @@ require "forwardable"
 module AoC2021
   # Calculates conditions of victory for given plays and boards
   class TrickShot
+    # Encapsulates calculations around a probe.
+    class Probe
+      def initialize(x_vel, y_vel)
+        @x_vel = x_vel
+        @y_vel = y_vel
+        @x_pos = @y_pos = 0
+        @max_y = 0
+      end
+
+      def step
+        @x_pos += @x_vel
+        @y_pos += @y_vel
+        @max_y = [@max_y, @y_pos].max
+        @x_vel = if @x_vel.positive?
+                   @x_vel - 1
+                 elsif @x_vel.negative?
+                   @x_vel + 1
+                 else
+                   0
+                 end
+        @y_vel -= 1
+      end
+    end
+
     def initialize(file)
-      @lines = VentLines.new(file)
+      @lines = file.readline(chomp: true).match(/target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/)
+      pp @lines
     end
 
     def highest_y = 45
