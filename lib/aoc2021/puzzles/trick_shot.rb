e8ae17fc3847999ca075a4d937d0838fa75a9ca4
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-17 13:48:57 -0500

    Finished Day 17 A

--- lib/aoc2021/puzzles/trick_shot.rb
+++ lib/aoc2021/puzzles/trick_shot.rb
@@ -20,8 +20,8 @@ module AoC2021
         @max_y = [@max_y, @y_pos].max
         @x_vel = if @x_vel.positive?
                    @x_vel - 1
-                 elsif @x_vel.negative?
-                   @x_vel + 1
+                   # elsif @x_vel.negative?
+                   #   @x_vel + 1
                  else
                    0
                  end
@@ -30,10 +30,41 @@ module AoC2021
     end
 
     def initialize(file)
-      @lines = file.readline(chomp: true).match(/target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/)
-      pp @lines
+      match_data      = file.readline(chomp: true).match(/target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/)
+      @target_x_range = Regexp.last_match(1).to_i..Regexp.last_match(2).to_i
+      @target_y_range = Regexp.last_match(3).to_i..Regexp.last_match(4).to_i
+      pp match_data, @target_x_range, @target_y_range
     end
 
-    def highest_y = 45
+    def highest_y
+      # find_possible_xs
+      max_y = @target_y_range.min.abs - 1 # min meaning lowest and farthest from zero
+      max_y * (max_y + 1) / 2
+    end
+
+    def find_possible_xs
+      pp @target_x_range
+      (0..@target_x_range.max).map do |this_x|
+        next nil unless this_x.positive?
+
+        next nil unless (1..this_x).sum >= @target_x_range.min
+
+        next [1] if @target_x_range.include? this_x
+
+        # 15 + 14 = 29
+        steps = this_x.downto(0).each_with_index.reduce([]) { |acc, (next_x, idx)|
+          # pp acc, next_x, idx
+          @target_x_range.include?((next_x..this_x).sum) ? acc << (idx + 1) : acc
+          if next_x.zero? and @target_x_range.include?((next_x..this_x).sum)
+            acc << Float::INFINITY
+          end
+          acc
+        }
+
+        next steps unless steps.empty?
+
+        nil
+      end
+    end
   end
 end
--- spec/aoc2021/trick_shot_spec.rb
+++ spec/aoc2021/trick_shot_spec.rb
@@ -21,5 +21,58 @@ RSpec.describe AoC2021::TrickShot do
         expect(subject.highest_y).to eq 45
       end
     end
+
+    context "with actual input" do
+      subject { AoC2021::TrickShot.new StringIO.new(<<~BITS) }
+        target area: x=241..275, y=-75..-49
+      BITS
+
+      it "reaches a height of 2775" do
+        expect(subject.highest_y).to eq 2775
+      end
+    end
+  end
+
+  describe "#find_possible_xs" do
+    context "with provided input" do
+      subject { AoC2021::TrickShot.new StringIO.new(<<~BITS) }
+        target area: x=20..30, y=-10..-5
+      BITS
+
+      it "finds the expected possible values of x" do
+        expect(subject.find_possible_xs)
+          .to eq [nil, #  0
+                  nil, #  1
+                  nil, #  2
+                  nil, #  3
+                  nil, #  4
+                  nil, #  5: 5 + 4 + 3 + 2 + 1 + 0 ... never reaches 20
+                  [5, 6, 7, Float::INFINITY], #  6: 6 + 5 + 4 + 3 + 2 = 20 + 1 = 21 + 0 = 21 to infinity
+                  [4, 5, 6, 7, 8, Float::INFINITY], #  7: 7 + 6 + 5 + 4 = 22 + 3 = 25 + 2 = 27 + 1 = 28 + 0 = 28 to infinity
+                  [3, 4, 5], #  8: 8 + 7 + 6 = 21 + 5 = 26 + 4 = 30
+                  [3, 4], #  9: 9 + 8 + 7 = 24 + 6 = 30
+                  [3], # 10: 10 + 9 + 8 = 27
+                  [2, 3], # 11: 11 + 10 = 21 + 9 = 30
+                  [2], # 12: 12 + 11 = 23
+                  [2], # 13: 13 + 12 = 25
+                  [2], # 14: 14 + 13 = 27
+                  [2], # 15: 15 + 14 = 29
+                  nil, # 16
+                  nil, # 17
+                  nil, # 18
+                  nil, # 19
+                  [1], # 20
+                  [1], # 21
+                  [1], # 22
+                  [1], # 23
+                  [1], # 24
+                  [1], # 25
+                  [1], # 26
+                  [1], # 27
+                  [1], # 28
+                  [1], # 29
+                  [1]] # 30
+      end
+    end
   end
 end
