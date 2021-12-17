2da45aa3aab7aa4c32a644983b66bafacda49220
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-17 15:32:20 -0500

    WIP - fixed some reeks

--- lib/aoc2021/puzzles/trick_shot.rb
+++ lib/aoc2021/puzzles/trick_shot.rb
@@ -5,29 +5,8 @@ require "forwardable"
 module AoC2021
   # Calculates conditions of victory for given plays and boards
   class TrickShot
-    # Encapsulates calculations around a probe.
-    class Probe
-      def initialize(x_vel, y_vel)
-        @x_vel = x_vel
-        @y_vel = y_vel
-        @x_pos = @y_pos = 0
-        @max_y = 0
-      end
-
-      def step
-        @x_pos += @x_vel
-        @y_pos += @y_vel
-        @max_y = [@max_y, @y_pos].max
-        @x_vel = if @x_vel.positive?
-                   @x_vel - 1
-                   # elsif @x_vel.negative?
-                   #   @x_vel + 1
-                 else
-                   0
-                 end
-        @y_vel -= 1
-      end
-    end
+    extend Forwardable
+    def_instance_delegators "self.class", :y_step
 
     def initialize(file)
       file.readline(chomp: true).match(/target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)/)
@@ -45,7 +24,7 @@ module AoC2021
     end
 
     # Returns the y position the probe will have after `step_number` number of steps.
-    def y_step(initial_y_velocity, step_number)
+    def self.y_step(initial_y_velocity, step_number)
       (0..(step_number - 1)).reduce(0) { |acc, step| acc + (initial_y_velocity - step) }
     end
 
@@ -54,7 +33,7 @@ module AoC2021
       x_steps.each_with_index.each_with_object(Set[]) do |(x_step, x_idx), acc|
         next acc unless x_step
 
-        (min_y..@target_y_range.min.abs - 1).each do |y_idx|
+        (min_y..min_y.abs - 1).each do |y_idx|
           try_each_y acc, min_y, x_idx, x_step, y_idx
         end
       end
@@ -63,8 +42,10 @@ module AoC2021
     def find_possible_xs
       (0..@target_x_range.max).map do |this_x|
         steps = this_x.downto(0).each_with_index.each_with_object([]) do |(next_x, idx), acc|
-          @target_x_range.include?((next_x..this_x).sum) ? acc << (idx + 1) : acc
-          acc << Float::INFINITY if next_x.zero? && @target_x_range.include?((next_x..this_x).sum)
+          next acc unless @target_x_range.include?((next_x..this_x).sum)
+
+          acc << (idx + 1)
+          acc << Float::INFINITY if next_x.zero?
         end
         next steps unless steps.empty?
 
