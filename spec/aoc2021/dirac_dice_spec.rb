d8947fa0bb3700e99eae4deb6e88f95ed5f0c360
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-21 10:28:51 -0500

    Set up for Day 21

---
+++ spec/aoc2021/dirac_dice_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "aoc2021/puzzles/trench_map"
+
+RSpec.describe AoC2021::DiracDice do
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+  describe "#lit_pixels_after" do
+    context "with first example" do
+      subject { AoC2021::DiracDice.new StringIO.new(<<~NUMBERS) }
+        Player 1 starting position: 4
+        Player 2 starting position: 8
+      NUMBERS
+
+      it "finds the expected number of lit pixels" do
+        expect(subject.lit_pixels_after(0)).to eq 10
+        expect(subject.lit_pixels_after(1)).to eq 24
+        expect(subject.lit_pixels_after(2)).to eq 35
+      end
+
+      it "finds the expected images after each enhancement" do
+        expect(subject.enhance(0)).to eq <<~PIXELS
+          #..#.
+          #....
+          ##..#
+          ..#..
+          ..###
+        PIXELS
+        expect(subject.enhance(1)).to eq <<~PIXELS
+          .##.##.
+          #..#.#.
+          ##.#..#
+          ####..#
+          .#..##.
+          ..##..#
+          ...#.#.
+        PIXELS
+        expect(subject.enhance(2)).to eq <<~PIXELS
+          .......#.
+          .#..#.#..
+          #.#...###
+          #...##.#.
+          #.....#.#
+          .#.#####.
+          ..#.#####
+          ...##.##.
+          ....###..
+        PIXELS
+      end
+    end
+  end
+end
