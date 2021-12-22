f16aa4f28e539f80f09161f83dd6d693e0e2e2df
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-21 19:03:08 -0500

    Finished Day 21 B

--- lib/aoc2021/puzzles/dirac_dice.rb
+++ lib/aoc2021/puzzles/dirac_dice.rb
@@ -10,15 +10,20 @@ module AoC2021
 
     def self.day21
       dirac_dice = File.open("input/day21a.txt") { |file| DiracDice.new file }
-      puts "Day 20, part A: #{ dirac_dice.lit_pixels_after(2) } pixels are lit in the resulting image."
-      # puts "Day 20, part B: #{dirac_dice.permutations} "
+      print "Day 21, part A: "
+      dirac_dice.first300
+      # print "Day 21, part B: "
+      # puts "#{dirac_dice.dirac_to_score(21)} "
       puts
     end
 
     def initialize(file = StringIO.new(""))
       @lines = file.readlines(chomp: true)
 
-      @roll_num = 0
+      @roll_num  = 0
+      @all_rolls = [1, 2, 3].repeated_permutation(3)
+                            .map(&:sum)
+                            .reduce(Hash.new { |hash, key| hash[key] = 0 }) { |acc, sum| acc.merge(sum => (1 + acc[sum])) }
     end
 
     def next_roll = @roll_num += 1
@@ -26,28 +31,66 @@ module AoC2021
     def first300
       player1_score = player2_score = 0
 
-      player1_pos  = 9 # 4
-      player2_pos  = 3 # 8
-      turn         = 1
+      player1_pos   = 9 # 4
+      player2_pos   = 3 # 8
+      player1s_turn = true
       while player1_score < 1000 && player2_score < 1000
         rolls              = (1..3).map { |_| next_roll % 100 }
         move_total_squares = rolls.sum
-        if turn == 1
+        if player1s_turn
           player1_pos   = (player1_pos + move_total_squares) % 10
           player1_pos   = 10 if player1_pos.zero?
           player1_score += player1_pos
-          puts "Player 1 rolls #{ rolls.map(&:to_s).join("+") } and moves to space #{ player1_pos } for a total score of #{ player1_score } after #{ @roll_num } total rolls"
+          # puts "Player 1 rolls #{ rolls.map(&:to_s).join("+") } and moves to space #{ player1_pos } for a total score of #{ player1_score_ } after #{ @roll_num } total rolls"
         else
           player2_pos   = (player2_pos + move_total_squares) % 10
           player2_pos   = 10 if player2_pos.zero?
           player2_score += player2_pos
-          puts "Player 2 rolls #{ rolls.map(&:to_s).join("+") } and moves to space #{ player2_pos } for a total score of #{ player2_score } after #{ @roll_num } total rolls"
+          # puts "Player 2 rolls #{ rolls.map(&:to_s).join("+") } and moves to space #{ player2_pos } for a total score of #{ player2_score } after #{ @roll_num } total rolls"
         end
-        turn               = (turn + 1) % 2
+
+        player1s_turn = !player1s_turn
       end
+
       losing_score = player2_score >= 1000 ? player1_score : player2_score
-      puts "The losing player had a score of #{ losing_score } when the game ended after #{ @roll_num } rolls."
-      puts "Their poduct is #{ losing_score * @roll_num }."
+      # puts "The losing player had a score of #{ losing_score } when the game ended after #{ @roll_num } rolls."
+      puts "#{ losing_score } * #{ @roll_num } = #{ losing_score * @roll_num }"
+    end
+
+    def dirac_to_score(win_score = 2)
+      @win_score = win_score
+      # wins       = roll_one_move 4, 8, 0, 0, true, [nil, 0, 0]  # with the given example
+      wins       = roll_one_move 9, 3, 0, 0, true, [nil, 0, 0]    # my puzzle input
+      puts "Player 1: #{ wins[1] } universes"
+      puts "Player 2: #{ wins[2] } universes"
+      wins
+    end
+
+    private
+
+    def roll_one_move(player1_pos, player2_pos, player1_score, player2_score, player1s_turn, wins, weight_so_far = 1)
+      @all_rolls.each do |roll_sum, weight|
+        if player1s_turn
+          player1_pos_   = ((player1_pos + roll_sum - 1) % 10) + 1
+          player1_score_ = player1_score + player1_pos_
+          # puts "P1 rolls #{ roll_sum }, moves fr #{ player1_pos } to #{ player1_pos_ }, score now #{ player1_score_ } wt #{ weight }"
+          if player1_score_ >= @win_score
+            wins[1] += weight * weight_so_far
+          else
+            wins = roll_one_move(player1_pos_, player2_pos, player1_score_, player2_score, false, wins, weight * weight_so_far)
+          end
+        else
+          player2_pos_   = ((player2_pos + roll_sum - 1) % 10) + 1
+          player2_score_ = player2_score + player2_pos_
+          # puts "  P2 rolls #{ roll_sum }, moves fr #{ player2_pos } to #{ player2_pos_ }, score now #{ player2_score_ } wt #{ weight }"
+          if player2_score_ >= @win_score
+            wins[2] += weight * weight_so_far
+          else
+            wins = roll_one_move(player1_pos, player2_pos_, player1_score, player2_score_, true, wins, weight * weight_so_far)
+          end
+        end
+      end
+      wins
     end
   end
 end
--- spec/aoc2021/dirac_dice_spec.rb
+++ spec/aoc2021/dirac_dice_spec.rb
@@ -21,4 +21,51 @@ RSpec.describe AoC2021::DiracDice do
       expect(subject.first300).to eq nil
     end
   end
+
+  describe "#dirac_to_score" do
+    subject { AoC2021::DiracDice.new StringIO.new(<<~NUMBERS) }
+      Player 1 starting position: 4
+      Player 2 starting position: 8
+    NUMBERS
+
+    it "gets the expected results for wins at 1 point" do
+      expect(subject.dirac_to_score(1)).to eq [nil, 27, 0]
+    end
+
+    it "gets the expected results for wins at 2 points" do
+      expect(subject.dirac_to_score(2)).to eq [nil, 183, 156]
+    end
+
+    it "gets the expected results for wins at 3 points" do
+      expect(subject.dirac_to_score(3)).to eq [nil, 990, 207]
+    end
+
+    it "gets the expected results for wins at 4 points" do
+      expect(subject.dirac_to_score(4)).to eq [nil, 2930, 971]
+    end
+
+    it "gets the expected results for wins at 5 points" do
+      expect(subject.dirac_to_score(5)).to eq [nil, 7907, 2728]
+    end
+
+    it "gets the expected results for wins at 6 points" do
+      expect(subject.dirac_to_score(6)).to eq [nil, 30_498, 7203]
+    end
+
+    it "gets the expected results for wins at 7 points" do
+      expect(subject.dirac_to_score(7)).to eq [nil, 127_019, 152_976]
+    end
+
+    it "gets the expected results for wins at 8 points" do
+      expect(subject.dirac_to_score(8)).to eq [nil, 655_661, 1_048_978]
+    end
+
+    it "gets the expected results for wins at 9 points" do
+      expect(subject.dirac_to_score(9)).to eq [nil, 4_008_007, 4_049_420]
+    end
+
+    it "gets the expected results for wins at 10 points" do
+      expect(subject.dirac_to_score(10)).to eq [nil, 18_973_591, 12_657_100]
+    end
+  end
 end
