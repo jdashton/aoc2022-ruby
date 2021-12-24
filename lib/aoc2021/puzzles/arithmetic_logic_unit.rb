1c7108b67174e2570ae37aeb96a5671969c97efb
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-24 14:44:06 -0500

    WIP intermediate translation

--- lib/aoc2021/puzzles/arithmetic_logic_unit.rb
+++ lib/aoc2021/puzzles/arithmetic_logic_unit.rb
@@ -25,34 +25,11 @@ module AoC2021
     WS = [5, 14, 15, 16, 8, 9, 2, 13, 16, 6, 6, 9, 11, 5].freeze
 
     def self.calculate_one(digit, w, z = 0)
-      # inp w
-      # mul x 0
-      # add x z
-      # mod x 26
-      # add x 13
-      # eql x w
-      # eql x 0
-      x = (z % 26 + XS[digit]) == w ? 0 : 1
-
-      # mul y 0
-      # add y 25
-      # mul y x
-      # add y 1
-      y = (25 * x) + 1
-
-      # div z 1 or 26
-      # mul z y
-      z = z / ZS[digit] * y
-
-      # mul y 0
-      # add y w
-      # add y 5
-      # mul y x
-      y = (w + WS[digit]) * x
-
-      # add z y
-      z += y
-      z
+      z = z / ZS[digit] # either 1 (bno-op) or 26 (pop)
+      z = z * 26 + w + WS[digit] if w != (z % 26 + XS[digit])
+      pp [digit, w, z]
     end
+
+    def self.one_place(place) = 9.downto(1) { |w| calculate_one(place, w, 0) }
   end
 end
