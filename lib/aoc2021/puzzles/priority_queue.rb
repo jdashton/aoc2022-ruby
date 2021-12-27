1498d2eb4c531d5fac1850ff482f9db57c2a16e6
J. Daniel Ashton <jdashton@ashtonfam.org>
2021-12-27 15:35:03 -0500

    First implementation of priority queue

---
+++ lib/aoc2021/puzzles/priority_queue.rb
@@ -0,0 +1,34 @@
+
+
+module AoC2021
+  class PriorityQueue
+    def initialize
+      @queue = []
+      @low_item = Float::INFINITY
+    end
+
+    def next
+      return nil if @low_item == Float::INFINITY
+
+      while @low_item < @queue.length
+        @queue[@low_item] = nil if @queue[@low_item]&.empty?
+        next @low_item += 1 unless @queue[@low_item]
+
+        item = @queue[@low_item].first
+        @queue[@low_item].delete(item)
+        return item
+      end
+    end
+
+    def <<(item)
+      weight = item[1]
+      @queue[weight] ||= Set[]
+      @queue[weight] << item
+      @low_item = [@low_item, weight].min
+    end
+
+    def +(collection)
+      collection.each { |item| self << item }
+    end
+  end
+end
---
+++ spec/aoc2021/priority_queue_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+RSpec.describe AoC2021::PriorityQueue do
+  describe "#next" do
+    context "with provided input" do
+      it "returns nil when the queue is empty" do
+        expect(subject.next).to be nil
+      end
+
+      it "returns the expected item" do
+        subject << [:a, 2, :c]
+        expect(subject.next).to eq [:a, 2, :c]
+      end
+    end
+  end
+
+  describe "#<<" do
+    it "adds a single item to the queue" do
+      subject << [:a, 2, :c]
+      expect(subject.next).to eq [:a, 2, :c]
+    end
+  end
+
+  describe "#+" do
+    it "adds two items to the queue" do
+      subject + [[:a, 7, :c], [:d, 2, :f]]
+      expect(subject.next).to eq [:d, 2, :f]
+      expect(subject.next).to eq [:a, 7, :c]
+      expect(subject.next).to be nil
+    end
+  end
+end
