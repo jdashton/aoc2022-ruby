90d9efeee6aafb6c1774d6c3d4b24ed58a5c2786
J. Daniel Ashton <jdashton@ashtonfam.org>
2022-01-06 16:22:53 -0500

    Re-implemented cube processing
    Now running in the 8 to 9 second range. Faster than the earlier 12 seconds,
    slower than the 4.5 seconds I had recently.

--- lib/aoc2021/puzzles/reactor_reboot.rb
+++ lib/aoc2021/puzzles/reactor_reboot.rb
@@ -6,96 +6,124 @@ module AoC2021
   # Rebooting cubes in the reactor for Day 22
   class ReactorReboot
     def self.day22
-      reactor_reboot = File.open("input/day22a.txt") { |file| ReactorReboot.new file }.process_volumes
-      puts "Day 22, part A: #{ reactor_reboot.cube_volume(-50..50) } cubes are on within the -50..50 space."
+      reactor_reboot = File.open("input/day22a.txt") { |file| ReactorReboot.new file }
+      puts "Day 22, part A: #{ reactor_reboot.cube_volume } cubes are on within the -50..50 space."
       puts "Day 22, part B: #{ reactor_reboot.cube_volume_unlimited } cubes are on."
       puts
     end
 
-    STEP_PAT = /(on|off) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)/
+    STEP_PAT = /(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)/
 
     def initialize(file = StringIO.new(""))
-      @raw_steps = file.readlines(chomp: true).map { |line| STEP_PAT.match(line, &method(:make_step)) }
-      @cubes     = nil
+      @cuboids     = CuboidList.new
+      @full_volume = @near_volume = 0
+      file.readlines(chomp: true)
+          .map { |line| STEP_PAT.match(line, &method(:make_and_process)) }
+          .each(&method(:process_cuboid))
     end
 
-    def make_step(match_data)
-      [match_data[1].to_sym, *match_data.captures[1..].map(&:to_i).each_slice(2).map { Range.new(*_1) }]
+    def make_and_process(match_data)
+      Cuboid.new(match_data[1] == "on", *match_data.captures[1..].map(&:to_i).each_slice(2).map { Range.new(*_1) })
     end
 
-    def process_volumes
-      @cubes = [@raw_steps.pop]
-      @raw_steps.reverse.each(&method(:process_one_cube))
-      self
+    def process_cuboid(cuboid)
+      # puts "Processing #{ cuboid }"
+      new_cuboids = if cuboid.lit
+                      @full_volume += cuboid.volume
+                      @near_volume += cuboid.near_volume
+                      [cuboid]
+                    else
+                      []
+                    end
+
+      @cuboids.each do |other_cuboid|
+        next unless cuboid.intersects? other_cuboid
+
+        cuboid_intersection = other_cuboid.intersection(cuboid)
+        # puts "  ..  Intersects with #{ other_cuboid }, adding #{ cuboid_intersection }"
+
+        @full_volume += cuboid_intersection.volume
+        @near_volume += cuboid_intersection.near_volume
+        new_cuboids.push(cuboid_intersection)
+      end
+      @cuboids.insert(new_cuboids)
+      # puts "Total volume of #{ @full_volume }."
+      # puts
     end
 
-    def cube_volume_unlimited
-      @cubes.reduce(0) do |acc, (state, x_range, y_range, z_range)|
-        next acc if state == :off
+    # Encapsulates operations on a list of cuboids
+    class CuboidList
+      extend Forwardable
+      def_instance_delegators :@list, :each
 
-        acc + (x_range.size * y_range.size * z_range.size)
+      def initialize
+        @list = []
+      end
+
+      def insert(other) = @list += other
+
+      def to_s = "CuboidList#{ @list }"
+
+      def inspect = "CuboidList#{ @list.inspect }"
+    end
+
+    # Encapsulates operations on a cube
+    class Cuboid
+      attr_reader :lit, :x_range, :y_range, :z_range
+
+      def initialize(lit, x_range, y_range, z_range)
+        @lit     = lit
+        @x_range = x_range
+        @y_range = y_range
+        @z_range = z_range
+        @volume  = @x_range.size * @y_range.size * @z_range.size
+      end
+
+      NEAR_SPACE = Cuboid.new(false, -50..50, -50..50, -50..50)
+
+      def to_s = "Cuboid[ lit: #{ @lit }, #{ @x_range }, #{ @y_range }, #{ @z_range } ]"
+
+      def volume = @lit ? @volume : (0 - @volume)
+
+      def near_volume = intersects?(NEAR_SPACE) ? volume : 0
+
+      def intersects?(other)
+        @x_range.begin <= other.x_range.end && @x_range.end >= other.x_range.begin &&
+          @y_range.begin <= other.y_range.end && @y_range.end >= other.y_range.begin &&
+          @z_range.begin <= other.z_range.end && @z_range.end >= other.z_range.begin
+      end
+
+      def intersection(other)
+        Cuboid.new(!@lit, make_x_range(other.x_range), make_y_range(other.y_range), make_z_range(other.z_range))
+      end
+
+      private
+
+      def make_x_range(x_range)
+        [@x_range.begin, x_range.begin].max..[@x_range.end, x_range.end].min
+      end
+
+      def make_y_range(y_range)
+        [@y_range.begin, y_range.begin].max..[@y_range.end, y_range.end].min
+      end
+
+      def make_z_range(z_range)
+        [@z_range.begin, z_range.begin].max..[@z_range.end, z_range.end].min
       end
     end
 
-    def cube_volume(limit)
-      @cubes.reduce(0) do |acc, (state, x_range, y_range, z_range)|
-        next acc if state == :off
+    def cube_volume = @near_volume
 
-        next acc unless ReactorReboot.overlaps?([nil, limit, limit, limit], [state, x_range, y_range, z_range])
+    def cube_volume_unlimited = @full_volume
 
-        acc + (x_range.size * y_range.size * z_range.size)
-      end
-    end
-
-    def self.overlaps?((_, c1x, c1y, c1z), (_, c2x, c2y, c2z))
-      c1x.end >= c2x.begin && c1x.begin <= c2x.end &&
-        c1y.end >= c2y.begin && c1y.begin <= c2y.end &&
-        c1z.end >= c2z.begin && c1z.begin <= c2z.end
-    end
-
-    def self.split((c1s, c1x, c1y, c1z), (_, c2x, c2y, c2z))
-      acc = []
-      if c1x.begin < c2x.begin
-        acc << [c1s, c1x.begin..(c2x.begin - 1), c1y, c1z]
-        c1x = c2x.begin..c1x.end
-      end
-      if c1x.end > c2x.end
-        acc << [c1s, (c2x.end + 1)..c1x.end, c1y, c1z]
-        c1x = c1x.begin..c2x.end
-      end
-      if c1y.begin < c2y.begin
-        acc << [c1s, c1x, c1y.begin..(c2y.begin - 1), c1z]
-        c1y = c2y.begin..c1y.end
-      end
-      if c1y.end > c2y.end
-        acc << [c1s, c1x, (c2y.end + 1)..c1y.end, c1z]
-        c1y = c1y.begin..c2y.end
-      end
-      acc << [c1s, c1x, c1y, c1z.begin..(c2z.begin - 1)] if c1z.begin < c2z.begin
-      acc << [c1s, c1x, c1y, (c2z.end + 1)..c1z.end] if c1z.end > c2z.end
-      acc
-    end
-
-    private
-
-    def detect_and_split(potential, cube)
-      potential.reduce([]) do |acc, pcube|
-        if ReactorReboot.overlaps?(pcube, cube)
-          acc + ReactorReboot.split(pcube, cube)
-        else
-          acc << pcube
-        end
-      end
-    end
-
-    def process_one_cube(new_cube)
-      potential = [new_cube]
-      # @cubes.each do |cube|
-      @cubes[(@cubes.bsearch_index { _1[1].end >= new_cube[1].begin })..].each do |cube|
-        potential = detect_and_split(potential, cube)
-      end
-      @cubes.insert(@cubes.bsearch_index { _1[1].end > new_cube[1].end } || @cubes.size, *potential)
-      @cubes.sort_by! { _1[1].end }
-    end
+    # def process_one_cube(new_cube)
+    #   potential = [new_cube]
+    #   # @cubes.each do |cube|
+    #   @cuboids[(@cuboids.bsearch_index { _1[1].end >= new_cube[1].begin })..].each do |cube|
+    #     potential = detect_and_split(potential, cube)
+    #   end
+    #   @cuboids.insert(@cuboids.bsearch_index { _1[1].end > new_cube[1].end } || @cuboids.size, *potential)
+    #   @cuboids.sort_by! { _1[1].end }
+    # end
   end
 end
