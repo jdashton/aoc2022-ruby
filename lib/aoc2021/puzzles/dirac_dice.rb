965c1994b2d224146a4b5db23bfb92df145f1bca
J. Daniel Ashton <jdashton@ashtonfam.org>
2022-01-15 13:37:41 -0500

    Adapt Dirac solution from vodik

--- lib/aoc2021/puzzles/dirac_dice.rb
+++ lib/aoc2021/puzzles/dirac_dice.rb
@@ -5,26 +5,69 @@ require "forwardable"
 module AoC2021
   # Image Enhancement for Day 20
   class DiracDice
-    extend Forwardable
-    def_instance_delegators "self.class", :parse_position, :calculate_new_pos_score_weight
-
     def self.day21
       dirac_dice = File.open("input/day21a.txt") { |file| DiracDice.new file }
       puts "Day 21, part A: #{ dirac_dice.deterministic_die }"
-      puts "Day 21, part B: Player 1: #{ (wins = dirac_dice.dirac_to_score(3))[0] } universes, Player 2: #{ wins[1] } universes"
+      puts "Day 21, part B: Player 1: #{ (wins = dirac_dice.dirac_to_score(21))[0] } universes, Player 2: #{ wins[1] } universes"
       puts
+    end
+
+    # Encapsulates player state
+    class Player
+      attr_reader :score, :position
+
+      def initialize(position, score = 0)
+        @position = position - 1
+        @score    = score
+      end
+
+      def advance(rolls)
+        @position = (@position + rolls) % 10
+        @score    += @position + 1
+      end
+
+      def position_adjusted = @position + 1
+
+      def inspect = "Player { position: #{ @position }, score: #{ @score } }"
+
+      def to_s = inspect
+    end
+
+    # Encapsulates packing and unpacking of game state
+    class State
+      attr_reader :player1, :player2
+
+      def initialize(player1, player2)
+        if player2
+          @player1 = player1
+          @player2 = player2
+        else
+          @player1, @player2 = unpack(player1)
+        end
+      end
+
+      def pack
+        ((((@player1.score * 10) + @player1.position) & 0xff) << 8) |
+          (((@player2.score * 10) + @player2.position) & 0xff)
+      end
+
+      def self.unpack(packed_state)
+        player1 = (packed_state >> 8) & 0xff
+        player2 = (packed_state & 0xff)
+        State.new(Player.new((player1 % 10) + 1, player1 / 10), Player.new((player2 % 10) + 1, player2 / 10))
+      end
+
+      def inspect = "State: player1=(#{ @player1.inspect }), player2=(#{ @player2.inspect })"
     end
 
     POS_PAT = /Player \d starting position: (\d+)/
 
     def initialize(file = StringIO.new(""))
-      @start_positions = file.readlines(chomp: true).map(&method(:parse_position)).map(&:to_i)
+      @start_positions = file.readlines(chomp: true)
+                             .map { POS_PAT.match(_1) { |md| md[1] } }
+                             .map(&:to_i)
 
-      @win_score = 2
-      @roll_num  = 0
-      @all_rolls = [1, 2, 3].repeated_permutation(3)
-                            .map(&:sum)
-                            .reduce(Hash.new { |hash, key| hash[key] = 0 }) { |acc, sum| acc.merge(sum => (1 + acc[sum])) }
+      @roll_num = 0
     end
 
     def next_roll = @roll_num += 1
@@ -52,43 +95,66 @@ module AoC2021
       end
 
       losing_score = player2_score >= 1000 ? player1_score : player2_score
-      # puts "The losing player had a score of #{ losing_score } when the game ended after #{ @roll_num } rolls."
       "#{ losing_score } * #{ @roll_num } = #{ losing_score * @roll_num }"
     end
 
+    # {3=>1, 4=>3, 5=>6, 6=>7, 7=>6, 8=>3, 9=>1}
+    ALL_ROLLS = [1, 2, 3].repeated_permutation(3)
+                         .map(&:sum)
+                         .reduce(Hash.new { |hash, key| hash[key] = 0 }) { |acc, sum| acc.merge(sum => (1 + acc[sum])) }
+
+    # This implementation based directly on the work of Simon Gomizelj (vodik on Github)
     def dirac_to_score(win_score = 2)
-      @win_score = win_score
-      roll_one_move(*@start_positions, 0, 0, [0, 0], 1, 0)
+      counter = Array.new(57314, 0)
+
+      wins1 = wins2 = 0
+
+      counter[State.new(Player.new(@start_positions.first), Player.new(@start_positions.last)).pack] = 1
+
+      loop do
+        dirty        = false
+        next_counter = Array.new(57314, 0)
+
+        counter.each_with_index do |state_qty, packed_state|
+          next unless state_qty > 0
+
+          state = State.unpack(packed_state)
+
+          ALL_ROLLS.each do |p1_roll, p1_qty|
+            p1_hits = state_qty * p1_qty
+            player1 = state.player1.dup
+            player1.advance(p1_roll)
+
+            if player1.score >= win_score
+              wins1 += p1_hits
+              next
+            end
+
+            ALL_ROLLS.each do |p2_roll, p2_qty|
+              p2_hits = p1_hits * p2_qty
+              player2 = state.player2.dup
+              player2.advance(p2_roll)
+
+              if player2.score >= win_score
+                wins2 += p2_hits
+                next
+              end
+
+              pack               = State.new(player1, player2).pack
+              next_counter[pack] += p2_hits
+              dirty              = true
+            end
+          end
+        end
+
+        break [wins1, wins2] unless dirty
+
+        counter = next_counter
+      end
     end
 
     def self.parse_position(line)
       POS_PAT.match(line) { |md| md[1] }
-    end
-
-    def self.calculate_new_pos_score_weight(active_player_pos, active_player_score, roll_data, weight_so_far)
-      roll_sum, weight        = roll_data
-      active_player_new_pos   = ((active_player_pos + roll_sum - 1) % 10) + 1
-      active_player_new_score = active_player_score + active_player_new_pos
-      weight_product          = weight * weight_so_far
-      [active_player_new_pos, active_player_new_score, weight_product]
-    end
-
-    private
-
-    def roll_one_move(active_player_pos, other_player_pos, active_player_score, other_player_score, wins,
-                      weight_so_far, player_num)
-      @all_rolls.reduce(0) do |_, roll_data|
-        active_player_new_pos, active_player_new_score, weight_product = calculate_new_pos_score_weight(active_player_pos,
-                                                                                                        active_player_score,
-                                                                                                        roll_data,
-                                                                                                        weight_so_far)
-        if active_player_new_score >= @win_score
-          wins.fill(wins[player_num] + weight_product, player_num, 1) # A form of assignment that returns self
-        else
-          roll_one_move(other_player_pos, active_player_new_pos, other_player_score, active_player_new_score, wins,
-                        weight_product, (player_num + 1) % 2)
-        end
-      end
     end
   end
 end
--- spec/aoc2021/dirac_dice_spec.rb
+++ spec/aoc2021/dirac_dice_spec.rb
@@ -1,71 +1,329 @@
 # frozen_string_literal: true
 
 require "aoc2021/puzzles/trench_map"
+include AoC2021
 
-RSpec.describe AoC2021::DiracDice do
-  before do
-    # Do nothing
-  end
-
-  after do
-    # Do nothing
-  end
-
-  describe "#first300" do
-    subject { AoC2021::DiracDice.new StringIO.new(<<~NUMBERS) }
+RSpec.describe DiracDice do
+  describe "#deterministic_die" do
+    subject { DiracDice.new StringIO.new(<<~NUMBERS) }
       Player 1 starting position: 4
       Player 2 starting position: 8
     NUMBERS
 
-    it "runs" do
-      expect(subject.deterministic_die).to be_a(String)
+    it "find an answer of 1073709" do
+      expect(subject.deterministic_die).to eq "897 * 1197 = 1073709"
+    end
+  end
+
+  describe "DiracDice::State" do
+    it "packs and unpacks as expected: (9, 0), (3, 0)" do
+      unpacked = DiracDice::State.unpack(DiracDice::State.new(DiracDice::Player.new(9), DiracDice::Player.new(3)).pack)
+      expect([unpacked.player1.position_adjusted,
+              unpacked.player1.score,
+              unpacked.player2.position_adjusted,
+              unpacked.player2.score])
+        .to eq [9, 0, 3, 0]
+    end
+
+    it "packs and unpacks as expected: (20, 5), (22, 2)" do
+      unpacked = DiracDice::State.unpack(DiracDice::State.new(DiracDice::Player.new(5, 20), DiracDice::Player.new(2, 22)).pack)
+      expect([unpacked.player1.position_adjusted,
+              unpacked.player1.score,
+              unpacked.player2.position_adjusted,
+              unpacked.player2.score])
+        .to eq [5, 20, 2, 22]
+    end
+
+    it "should pack/unpack as expected" do
+      (0..20).each do |p1_score|
+        (1..10).each do |p1_pos|
+          (0..20).each do |p2_score|
+            (1..10).each do |p2_pos|
+              unpacked = DiracDice::State.unpack(DiracDice::State.new(DiracDice::Player.new(p1_pos, p1_score), DiracDice::Player.new(p2_pos, p2_score)).pack)
+              expect([unpacked.player1.position_adjusted,
+                      unpacked.player1.score,
+                      unpacked.player2.position_adjusted,
+                      unpacked.player2.score])
+                .to eq [p1_pos, p1_score, p2_pos, p2_score]
+            end
+          end
+        end
+      end
     end
   end
 
   describe "#dirac_to_score" do
-    subject { AoC2021::DiracDice.new StringIO.new(<<~NUMBERS) }
-      Player 1 starting position: 4
-      Player 2 starting position: 8
-    NUMBERS
+    context "with example input" do
+      subject { DiracDice.new StringIO.new(<<~NUMBERS) }
+        Player 1 starting position: 4
+        Player 2 starting position: 8
+      NUMBERS
 
-    it "gets the expected results for wins at 1 point" do
-      expect(subject.dirac_to_score(1)).to eq [27, 0]
+      it "gets the expected results for wins at 1 point" do
+        expect(subject.dirac_to_score(1)).to eq [27, 0]
+      end
+
+      it "gets the expected results for wins at 2 points" do
+        expect(subject.dirac_to_score(2)).to eq [183, 156]
+      end
+
+      it "gets the expected results for wins at 3 points" do
+        expect(subject.dirac_to_score(3)).to eq [990, 207]
+      end
+
+      it "gets the expected results for wins at 4 points" do
+        expect(subject.dirac_to_score(4)).to eq [2930, 971]
+      end
+
+      it "gets the expected results for wins at 5 points" do
+        expect(subject.dirac_to_score(5)).to eq [7907, 2728]
+      end
+
+      it "gets the expected results for wins at 6 points" do
+        expect(subject.dirac_to_score(6)).to eq [30_498, 7203]
+      end
+
+      it "gets the expected results for wins at 7 points" do
+        expect(subject.dirac_to_score(7)).to eq [127_019, 152_976]
+      end
+
+      it "gets the expected results for wins at 8 points" do
+        expect(subject.dirac_to_score(8)).to eq [655_661, 1_048_978]
+      end
+
+      it "gets the expected results for wins at 9 points" do
+        expect(subject.dirac_to_score(9)).to eq [4_008_007, 4_049_420]
+      end
+
+      it "gets the expected results for wins at 10 points" do
+        expect(subject.dirac_to_score(10)).to eq [18_973_591, 12_657_100]
+      end
+
+      it "gets the expected results for wins at 11 point" do
+        expect(subject.dirac_to_score(11)).to eq [90_197_150, 47_304_735]
+      end
+
+      it "gets the expected results for wins at 12 points" do
+        expect(subject.dirac_to_score(12)).to eq [454_323_519, 217_150_220]
+      end
+
+      it "gets the expected results for wins at 13 points" do
+        expect(subject.dirac_to_score(13)).to eq [2_159_295_972, 1_251_104_269]
+      end
+
+      it "gets the expected results for wins at 14 points" do
+        expect(subject.dirac_to_score(14)).to eq [9_632_852_745, 7_543_855_038]
+      end
+
+      it "gets the expected results for wins at 15 points" do
+        expect(subject.dirac_to_score(15)).to eq [43_413_388_231, 37_334_719_860]
+      end
+
+      it "gets the expected results for wins at 16 points" do
+        expect(subject.dirac_to_score(16)).to eq [199_092_281_721, 161_946_691_198]
+      end
+
+      it "gets the expected results for wins at 17 points" do
+        expect(subject.dirac_to_score(17)).to eq [903_307_715_712, 698_632_570_521]
+      end
+
+      it "gets the expected results for wins at 18 points" do
+        expect(subject.dirac_to_score(18)).to eq [4_227_532_541_969, 3_151_502_992_942]
+      end
+
+      it "gets the expected results for wins at 19 points" do
+        expect(subject.dirac_to_score(19)).to eq [20_259_464_849_183, 14_795_269_706_204]
+      end
+
+      it "gets the expected results for wins at 20 points" do
+        expect(subject.dirac_to_score(20)).to eq [95_627_706_087_732, 71_421_811_355_805]
+      end
+
+      it "gets the expected results for wins at 21 points" do
+        expect(subject.dirac_to_score(21)).to eq [444_356_092_776_315, 341_960_390_180_808]
+      end
     end
 
-    it "gets the expected results for wins at 2 points" do
-      expect(subject.dirac_to_score(2)).to eq [183, 156]
+    context "with vodik's input" do
+      subject { AoC2021::DiracDice.new StringIO.new(<<~NUMBERS) }
+        Player 1 starting position: 8
+        Player 2 starting position: 1
+      NUMBERS
+
+      it "gets the expected results for wins at 1 point" do
+        expect(subject.dirac_to_score(1)).to eq [27, 0]
+      end
+
+      it "gets the expected results for wins at 2 points" do
+        expect(subject.dirac_to_score(2)).to eq [26, 27]
+      end
+
+      it "gets the expected results for wins at 3 points" do
+        expect(subject.dirac_to_score(3)).to eq [23, 108]
+      end
+
+      it "gets the expected results for wins at 4 points" do
+        expect(subject.dirac_to_score(4)).to eq [17, 270]
+      end
+
+      it "gets the expected results for wins at 5 points" do
+        expect(subject.dirac_to_score(5)).to eq [448, 1009]
+      end
+
+      it "gets the expected results for wins at 6 points" do
+        expect(subject.dirac_to_score(6)).to eq [13_548, 7669]
+      end
+
+      it "gets the expected results for wins at 7 points" do
+        expect(subject.dirac_to_score(7)).to eq [115_461, 42_802]
+      end
+
+      it "gets the expected results for wins at 8 points" do
+        expect(subject.dirac_to_score(8)).to eq [481_421, 214_834]
+      end
+
+      it "gets the expected results for wins at 9 points" do
+        expect(subject.dirac_to_score(9)).to eq [1_237_307, 1_063_512]
+      end
+
+      it "gets the expected results for wins at 10 points" do
+        expect(subject.dirac_to_score(10)).to eq [3_498_202, 4_381_099]
+      end
+
+      it "gets the expected results for wins at 11 point" do
+        expect(subject.dirac_to_score(11)).to eq [14_263_794, 20_672_043]
+      end
+
+      it "gets the expected results for wins at 12 points" do
+        expect(subject.dirac_to_score(12)).to eq [83_427_348, 108_902_869]
+      end
+
+      it "gets the expected results for wins at 13 points" do
+        expect(subject.dirac_to_score(13)).to eq [559_035_775, 536_346_228]
+      end
+
+      it "gets the expected results for wins at 14 points" do
+        expect(subject.dirac_to_score(14)).to eq [2_997_705_145, 2_453_492_582]
+      end
+
+      it "gets the expected results for wins at 15 points" do
+        expect(subject.dirac_to_score(15)).to eq [12_973_782_795, 11_236_685_280]
+      end
+
+      it "gets the expected results for wins at 16 points" do
+        expect(subject.dirac_to_score(16)).to eq [52_668_728_921, 50_451_713_310]
+      end
+
+      it "gets the expected results for wins at 17 points" do
+        expect(subject.dirac_to_score(17)).to eq [227_421_283_105, 229_720_170_158]
+      end
+
+      it "gets the expected results for wins at 18 points" do
+        expect(subject.dirac_to_score(18)).to eq [1_050_229_675_698, 1_106_264_797_891]
+      end
+
+      it "gets the expected results for wins at 19 points" do
+        expect(subject.dirac_to_score(19)).to eq [5_050_819_482_983, 5_373_953_995_748]
+      end
+
+      it "gets the expected results for wins at 20 points" do
+        expect(subject.dirac_to_score(20)).to eq [24_348_664_990_294, 25_311_443_930_831]
+      end
+
+      it "gets the expected results for wins at 21 points" do
+        expect(subject.dirac_to_score(21)).to eq [113_467_910_521_040, 116_741_133_558_209]
+      end
     end
 
-    it "gets the expected results for wins at 3 points" do
-      expect(subject.dirac_to_score(3)).to eq [990, 207]
-    end
+    context "with my actual input" do
+      subject { AoC2021::DiracDice.new StringIO.new(<<~NUMBERS) }
+        Player 1 starting position: 9
+        Player 2 starting position: 3
+      NUMBERS
 
-    it "gets the expected results for wins at 4 points" do
-      expect(subject.dirac_to_score(4)).to eq [2930, 971]
-    end
+      it "gets the expected results for wins at 1 point" do
+        expect(subject.dirac_to_score(1)).to eq [27, 0]
+      end
 
-    it "gets the expected results for wins at 5 points" do
-      expect(subject.dirac_to_score(5)).to eq [7907, 2728]
-    end
+      it "gets the expected results for wins at 2 points" do
+        expect(subject.dirac_to_score(2)).to eq [27, 0]
+      end
 
-    it "gets the expected results for wins at 6 points" do
-      expect(subject.dirac_to_score(6)).to eq [30_498, 7203]
-    end
+      it "gets the expected results for wins at 3 points" do
+        expect(subject.dirac_to_score(3)).to eq [134, 23]
+      end
 
-    it "gets the expected results for wins at 7 points" do
-      expect(subject.dirac_to_score(7)).to eq [127_019, 152_976]
-    end
+      it "gets the expected results for wins at 4 points" do
+        expect(subject.dirac_to_score(4)).to eq [478, 199]
+      end
 
-    it "gets the expected results for wins at 8 points" do
-      expect(subject.dirac_to_score(8)).to eq [655_661, 1_048_978]
-    end
+      it "gets the expected results for wins at 5 points" do
+        expect(subject.dirac_to_score(5)).to eq [1327, 1300]
+      end
 
-    it "gets the expected results for wins at 9 points" do
-      expect(subject.dirac_to_score(9)).to eq [4_008_007, 4_049_420]
-    end
+      it "gets the expected results for wins at 6 points" do
+        expect(subject.dirac_to_score(6)).to eq [6942, 5487]
+      end
 
-    it "gets the expected results for wins at 10 points" do
-      expect(subject.dirac_to_score(10)).to eq [18_973_591, 12_657_100]
+      it "gets the expected results for wins at 7 points" do
+        expect(subject.dirac_to_score(7)).to eq [44_664, 15_657]
+      end
+
+      it "gets the expected results for wins at 8 points" do
+        expect(subject.dirac_to_score(8)).to eq [263_453, 56_842]
+      end
+
+      it "gets the expected results for wins at 9 points" do
+        expect(subject.dirac_to_score(9)).to eq [1_305_936, 311_993]
+      end
+
+      it "gets the expected results for wins at 10 points" do
+        expect(subject.dirac_to_score(10)).to eq [5_666_175, 2_248_356]
+      end
+
+      it "gets the expected results for wins at 11 point" do
+        expect(subject.dirac_to_score(11)).to eq [25_717_855, 17_012_236]
+      end
+
+      it "gets the expected results for wins at 12 points" do
+        expect(subject.dirac_to_score(12)).to eq [130_653_171, 93_370_136]
+      end
+
+      it "gets the expected results for wins at 13 points" do
+        expect(subject.dirac_to_score(13)).to eq [676_634_886, 412_321_607]
+      end
+
+      it "gets the expected results for wins at 14 points" do
+        expect(subject.dirac_to_score(14)).to eq [3_218_069_724, 1_672_421_285]
+      end
+
+      it "gets the expected results for wins at 15 points" do
+        expect(subject.dirac_to_score(15)).to eq [14_665_819_559, 7_078_438_276]
+      end
+
+      it "gets the expected results for wins at 16 points" do
+        expect(subject.dirac_to_score(16)).to eq [66_652_310_663, 32_351_952_124]
+      end
+
+      it "gets the expected results for wins at 17 points" do
+        expect(subject.dirac_to_score(17)).to eq [300_196_560_960, 160_116_009_889]
+      end
+
+      it "gets the expected results for wins at 18 points" do
+        expect(subject.dirac_to_score(18)).to eq [1_394_336_687_349, 803_898_066_354]
+      end
+
+      it "gets the expected results for wins at 19 points" do
+        expect(subject.dirac_to_score(19)).to eq [6_701_087_542_685, 3_902_175_769_490]
+      end
+
+      it "gets the expected results for wins at 20 points" do
+        expect(subject.dirac_to_score(20)).to eq [31_950_745_840_868, 18_648_052_566_173]
+      end
+
+      it "gets the expected results for wins at 21 points" do
+        expect(subject.dirac_to_score(21)).to eq [148_747_830_493_442, 89_305_072_914_203]
+      end
     end
   end
 end
