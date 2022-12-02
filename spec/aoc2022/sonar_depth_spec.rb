edfb55b64ba1a38497b3d8a457f5f0226c3480b2
J. Daniel Ashton <jdashton@ashtonfam.org>
2022-12-02 18:54:20 -0500

    Tweak specs for Rubocop

--- spec/aoc2022/sonar_depth_spec.rb
+++ spec/aoc2022/sonar_depth_spec.rb
@@ -5,19 +5,19 @@ require "aoc2022/puzzles/sonar_depth"
 RSpec.describe AoC2022::SonarDepth do
   describe "#count_increases" do
     context "with [1, 3, 2]" do
-      subject { described_class.new StringIO.new(<<~NUMBERS) }
+      subject(:sonar_depth) { described_class.new StringIO.new(<<~NUMBERS) }
         1
         3
         2
       NUMBERS
 
       it "counts 1 increase" do
-        expect(subject.count_increases).to eq 1
+        expect(sonar_depth.count_increases).to eq 1
       end
     end
 
     context "with [1, 3, 2, 4]" do
-      subject { described_class.new StringIO.new(<<~NUMBERS) }
+      subject(:sonar_depth) { described_class.new StringIO.new(<<~NUMBERS) }
         1
         3
         2
@@ -25,12 +25,12 @@ RSpec.describe AoC2022::SonarDepth do
       NUMBERS
 
       it "counts 2 increases" do
-        expect(subject.count_increases).to eq 2
+        expect(sonar_depth.count_increases).to eq 2
       end
     end
 
     context "with provided test data" do
-      subject { described_class.new StringIO.new(<<~NUMBERS) }
+      subject(:sonar_depth) { described_class.new StringIO.new(<<~NUMBERS) }
         199
         200
         208
@@ -44,14 +44,14 @@ RSpec.describe AoC2022::SonarDepth do
       NUMBERS
 
       it "counts 7 increases" do
-        expect(subject.count_increases).to eq 7
+        expect(sonar_depth.count_increases).to eq 7
       end
     end
   end
 
   describe "#count_triplet_increases" do
     context "with [1, 3, 2, 4, 3, 5]" do
-      subject { described_class.new StringIO.new(<<~NUMBERS) }
+      subject(:sonar_depth) { described_class.new StringIO.new(<<~NUMBERS) }
         1
         3
         2
@@ -61,12 +61,12 @@ RSpec.describe AoC2022::SonarDepth do
       NUMBERS
 
       it "counts 2 increases" do
-        expect(subject.count_triplet_increases).to eq 2
+        expect(sonar_depth.count_triplet_increases).to eq 2
       end
     end
 
     context "with [1, 3, 2, 4, 3, 0]" do
-      subject { described_class.new StringIO.new(<<~NUMBERS) }
+      subject(:sonar_depth) { described_class.new StringIO.new(<<~NUMBERS) }
         1
         3
         2
@@ -76,12 +76,12 @@ RSpec.describe AoC2022::SonarDepth do
       NUMBERS
 
       it "counts 1 increase" do
-        expect(subject.count_triplet_increases).to eq 1
+        expect(sonar_depth.count_triplet_increases).to eq 1
       end
     end
 
     context "with [1, 3, 2, 4, 3, 5, 4]" do
-      subject { described_class.new StringIO.new(<<~NUMBERS) }
+      subject(:sonar_depth) { described_class.new StringIO.new(<<~NUMBERS) }
         1
         3
         2
@@ -92,12 +92,12 @@ RSpec.describe AoC2022::SonarDepth do
       NUMBERS
 
       it "counts 2 increases" do
-        expect(subject.count_triplet_increases).to eq 2
+        expect(sonar_depth.count_triplet_increases).to eq 2
       end
     end
 
     context "with provided test data" do
-      subject { described_class.new StringIO.new(<<~NUMBERS) }
+      subject(:sonar_depth) { described_class.new StringIO.new(<<~NUMBERS) }
         199
         200
         208
@@ -111,7 +111,7 @@ RSpec.describe AoC2022::SonarDepth do
       NUMBERS
 
       it "counts 5 increases" do
-        expect(subject.count_triplet_increases).to eq 5
+        expect(sonar_depth.count_triplet_increases).to eq 5
       end
     end
   end
