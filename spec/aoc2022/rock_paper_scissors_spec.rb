daa24cf3c8c7fd19a4f3853f7c6b960c3e22b804
J. Daniel Ashton <jdashton@ashtonfam.org>
2022-12-04 21:06:30 -0500

    Day 2 complete

---
+++ spec/aoc2022/rock_paper_scissors_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "aoc2022/puzzles/elf_calories"
+
+RSpec.describe AoC2022::RockPaperScissors do
+  context "with provided test data" do
+    subject(:rock_paper_scissors) { described_class.new StringIO.new(<<~DATA) }
+      A Y
+      B X
+      C Z
+    DATA
+
+    it "finds 15 as the score" do
+      expect(rock_paper_scissors.guide_alpha).to eq 15
+    end
+
+    it "finds 12 as the score with better understanding" do
+      expect(rock_paper_scissors.guide_beta).to eq 12
+    end
+  end
+end
