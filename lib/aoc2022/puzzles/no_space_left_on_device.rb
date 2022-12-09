6f3dbb5337f2a7091e98d7cf8bf7eda2e30fd596
J. Daniel Ashton <jdashton@ashtonfam.org>
2022-12-09 09:35:18 -0500

    Day 7 complete

---
+++ lib/aoc2022/puzzles/no_space_left_on_device.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+module AoC2022
+  module Puzzles
+    # For Day 6, we're looking for message headers
+    class NoSpaceLeftOnDevice
+      def self.day07
+        dev_fs = File.open("input/day07.txt") { |file| NoSpaceLeftOnDevice.new file }
+        puts "Day  7, part A: The sum of directory sizes is #{ dev_fs.small_dir_sum }."
+        # puts "Day  6, part B: The message marker ends at character #{ datastream.message_marker }."
+        puts
+      end
+
+      Node = Data.define(:name, :parent, :size, :dirs)
+
+      def initialize(file)
+        temp_root   = Node.new("/", nil, 0, [])
+        current_dir = nil
+        @lines      = file
+                        .readlines(chomp: true)
+                        .slice_before(/\A\$/)
+                        .map { |slice|
+                          # pp slice
+                          # noinspection RubyEmptyElseBlockInspection
+                          case slice[0][2..3]
+                            when "cd" then slice[0][5..]
+                            when "ls" then parse_dir slice[1..]
+                            else
+                          end
+                        }
+      end
+
+      # Returns a list of directories in this directory, and the size of the files visible in this directory.
+      def parse_dir(ary)
+        ary.reduce([0]) { |acc, entry| entry.start_with?("dir ") ? acc << entry[4..] : acc[0] += entry.to_i; acc }
+        # ary.reduce([0]) { |acc, entry| pp [acc, entry]; entry.start_with?("dir ") ? acc << entry[4..] : acc[0] += entry.to_i; pp [acc, entry]; acc }
+      end
+
+      def self.process(_dir_name, contents, *remainder, dir_size_list)
+        size = contents[0]
+
+        until remainder.nil? || remainder[0].nil? || remainder[0] == ".." do
+          subdir_size, updated_list, remainder = process(*remainder, dir_size_list)
+          size                                 += subdir_size
+          # dir_size_list                        += updated_list
+        end
+
+        return [size, dir_size_list << size, remainder.nil? ? nil : remainder[1..]]
+      end
+
+      def self.sum_directories(list)
+        # ["/", [23352670, "a", "d"], "a", [94269, "e"], "e", [584], "..", "..", "d", [24933642]]
+        process(*list, [])
+      end
+
+      def small_dir_sum
+        NoSpaceLeftOnDevice.sum_directories(@lines)[1].filter { |dir| dir <= 100_000 }.sum
+      end
+
+      def smallest_large_enough
+        sum_of_directories = NoSpaceLeftOnDevice.sum_directories(@lines)
+        needed             = 30000000 - (70000000 - sum_of_directories[0])
+        sum_of_directories[1].tap { |s| pp s }.sort.tap { |s| pp s }.find { |elt| elt >= needed }
+      end
+    end
+  end
+end
