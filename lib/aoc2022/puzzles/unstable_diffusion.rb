25caff02d9e889a9b1f5d025f58d8540074c97ef
J. Daniel Ashton <jdashton@ashtonfam.org>
2022-12-27 22:22:49 -0500

    Day 23 complete

--- lib/aoc2022/puzzles/unstable_diffusion.rb
+++ lib/aoc2022/puzzles/unstable_diffusion.rb
@@ -11,9 +11,29 @@ module AoC2022
         puts
       end
 
+      DIRS = {
+        north: ->(x, y) { [x, y - 1] },
+        south: ->(x, y) { [x, y + 1] },
+        west:  ->(x, y) { [x - 1, y] },
+        east:  ->(x, y) { [x + 1, y] }
+      }.freeze
+
       # :reek:FeatureEnvy
       def initialize(file)
-        @lines = file.readlines(chomp: true)
+        @dirs  = DIRS.keys
+        @board = file
+                   .readlines(chomp: true)
+                   .each_with_index
+                   .reduce({}) { |line_acc, (line, y)|
+                     # pp [line_acc, line, y_prime]
+                     line
+                       .chars
+                       .each_with_index
+                       .reduce(line_acc) { |acc, (char, x)|
+                         # pp [acc, char, x_prime]
+                         acc.merge(char == '#' ? { [x, y] => '#' } : {})
+                       }
+                   }
       end
 
       # Round:
@@ -32,14 +52,126 @@ module AoC2022
       # Finally, rotate the direction sequence, so that S is considered first in the 2nd round,
       # west in the 3rd round and so forth.
       #   directions = [:north, :south, :west, :east].rotate
+      def render
+        # puts " -- Rendering #{ @board }"
+        xs, ys = [[], []]
+        @board.each { |(x, y), _| xs << x; ys << y }
 
-      def propose_moves(board, directions)
-        pp board
-        pp directions
+        Range.new(*ys.minmax).map { |y|
+          Range.new(*xs.minmax).map { |x|
+            @board[[x, y]] || '.'
+          }.join
+        }.join("\n") + "\n"
+      end
+
+      def lonely?((x, y))
+        # puts "checking lonely"
+        ((y - 1)..(y + 1)).map do |y_prime|
+          ((x - 1)..(x + 1)).map do |x_prime|
+            next nil if x_prime == x && y_prime == y
+            @board[[x_prime, y_prime]]
+          end
+        end.flatten.all?(&:nil?)
+      end
+
+      def empty_dir?((x, y), dir)
+        # pp [x, y, dir]
+        new_x, new_y = DIRS[dir].call(x, y)
+        if dir in :north | :south
+          [[x - 1, new_y], [x, new_y], [x + 1, new_y]]
+        else
+          [[new_x, y - 1], [new_x, y], [new_x, y + 1]]
+        end.all? { |pos| @board[pos].nil? }
+      end
+
+      def propose_moves
+        # puts "Starting propose_moves"
+        # pp @board
+
+        num_moves = 0
+        @proposed = @board.reduce({}) do |acc, (elf, val)|
+          # puts "New elf: #{ acc }, #{ elf }, #{ val }"
+          if lonely?(elf)
+            # puts "found a lonely elf at #{ elf }"
+            acc.merge({ elf => val })
+          else
+            @dirs.reduce(acc) do |dir_acc, dir|
+              # puts "trying #{ [dir] }"
+              if empty_dir?(elf, dir)
+                # puts "found a direction to move with #{ elf } going #{ dir }"
+                new_pos = DIRS[dir].call(*elf)
+                # pp [new_pos, dir_acc, elf]
+                num_moves += 1
+                break dir_acc.merge({ new_pos => ((dir_acc[new_pos] || []) << elf) })
+              else
+                if dir == @dirs.last
+                  dir_acc.merge({ elf => val })
+                else
+                  dir_acc
+                end
+              end
+            end
+          end
+        end
+        num_moves
+      end
+
+      def actuate_moves
+        @board = {}
+        # pp @proposed
+        @proposed.each do |key, props|
+          case props
+            in "#"
+              # puts "Elf succeeds in moving to #{ key }"
+              @board[key] = props
+            in Array
+              if props.length == 1
+                # puts "Elf succeeds in moving to #{ key }"
+                @board[key] = "#"
+              else
+                # puts "#{ props.length } Elves cannot move to #{ key }"
+                props.each { |pos| @board[pos] = "#" }
+              end
+            else
+              # puts "  --  UNEXPECTED PROPOSAL: #{ key } => #{ props }"
+          end
+        end
+      end
+
+      def spread_out(max_rounds = 10)
+        @rounds = 1
+        # puts "\n -- starting Round #{ @rounds }\n\n"
+        while propose_moves.positive?
+          @rounds += 1
+          actuate_moves
+          @dirs = @dirs.rotate
+          break if @rounds > max_rounds
+          # puts "\n -- starting Round #{ @rounds }\n\n"
+        end
+        self
+      end
+
+      def count_spaces
+        # puts " -- Rendering #{ @board }"
+        xs, ys = [[], []]
+        @board.each { |(x, y), _| xs << x; ys << y }
+        spaces = 0
+
+        Range.new(*ys.minmax).map { |y|
+          Range.new(*xs.minmax).map { |x|
+            @board[[x, y]].nil? && (spaces += 1)
+          }
+        }
+        spaces
       end
 
       def part_one
-        110
+        spread_out.count_spaces
+      end
+
+      def part_two
+        spread_out(1500)
+        @rounds
       end
     end
   end
--- spec/aoc2022/puzzles/unstable_diffusion_spec.rb
+++ spec/aoc2022/puzzles/unstable_diffusion_spec.rb
@@ -12,38 +12,75 @@ RSpec.describe AoC2022::Puzzles::UnstableDiffusion do
       .#..#..
     DATA
 
-    # .....
-    # ..##.
-    # ..#..
-    # .....
-    # ..##.
-    # .....
+    it 'finds this final state' do
+      expect(unstable_diffusion.spread_out.render).to eq <<~DATA
+        ......#.....
+        ..........#.
+        .#.#..#.....
+        .....#......
+        ..#.....#..#
+        #......##...
+        ....##......
+        .#........#.
+        ...#.#..#...
+        ............
+        ...#..#..#..
+      DATA
+    end
 
-    it 'finds 110 as the number of empty ground tiles' do
+    it 'finds 110 as the number of empty ground tiles after 10 rounds' do
       expect(unstable_diffusion.part_one).to eq 110
+    end
+
+    it 'finds 20 as the first round with no moves' do
+      expect(unstable_diffusion.part_two).to eq 20
     end
   end
 
   context 'with small test data' do
     subject(:unstable_diffusion) { described_class.new StringIO.new(<<~DATA) }
-      ..#..
-      ....#
-      #....
-      ....#
       .....
+      ..##.
       ..#..
+      .....
+      ..##.
+      .....
     DATA
 
-    it 'finds 110 as the number of empty ground tiles' do
-      expect(unstable_diffusion.propose_moves(board, directions)).to eq 110
+    it 'renders the input as expected' do
+      expect(unstable_diffusion.render).to eq <<~DATA
+        ##
+        #.
+        ..
+        ##
+      DATA
+    end
+
+    it 'finds this final state' do
+      expect(unstable_diffusion.spread_out.render).to eq <<~DATA
+        ..#..
+        ....#
+        #....
+        ....#
+        .....
+        ..#..
+      DATA
+    end
+
+    it "finds 25 as the number of spaces in the final state" do
+      expect(unstable_diffusion.spread_out.count_spaces).to eq 25
     end
   end
 
   context 'with actual input data' do
     subject(:unstable_diffusion) { File.open('input/day23.txt') { |file| described_class.new file } }
 
-    it 'finds 110 as the number of empty ground tiles' do
-      expect(unstable_diffusion.part_one).to eq 110
+    it 'finds 4116 as the number of empty ground tiles after 10 rounds' do
+      expect(unstable_diffusion.part_one).to eq 4116
+    end
+
+    it 'finds 984 as the first round with no moves' do
+      expect(unstable_diffusion.part_two).to eq 984
     end
   end
 end
