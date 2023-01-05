b791716266a5e031a0a6adf4fc226a5940f63f56
J. Daniel Ashton <jdashton@ashtonfam.org>
2023-01-05 09:34:35 -0500

    Time under 700 ms for Day 23

--- lib/aoc2022/puzzles/unstable_diffusion.rb
+++ lib/aoc2022/puzzles/unstable_diffusion.rb
@@ -38,9 +38,7 @@ module AoC2022
       def trim
         @board.shift while @board.first.zero?
         @board.pop while @board.last.zero?
-        while @board.all? { _1 & 1 == 0 }
-          @board = @board.map { _1 >> 1 }
-        end
+        @board = @board.map { _1 >> 1 } while @board.all? { (_1 & 1).zero? }
         self
       end
 
@@ -51,148 +49,113 @@ module AoC2022
         "#{ @board.map { _1.to_s(2).tr('01', '.#').rjust(max_len, '.') }.join("\n") }\n"
       end
 
+      # Reimplemented with bit strings, adapted from
+      #  https://github.com/SLiV9/AdventOfCode2022/blob/3f770dfb2c511249f9b16aface949d88f3d059b4/src/bin/day23/main.rs#L443
+      #
       def propose_and_move
         # Expand board as needed
-        while @board.any? { _1 & 1 == 1 }
-          @board = @board.map { _1 << 1 }
-        end
+        @board = @board.map { _1 << 1 } while @board.any? { _1 & 1 == 1 }
         @board.unshift(0) unless @board.first.zero?
         @board.push(0) unless @board.last.zero?
         @board.push(0) unless @board[-2].zero?
 
-        # RUST self.is_active = false;
-        any_elf_moved = false
+        any_elf_moved       = false
+        two_prev_prop_south = prev_prop_stay = prev_prop_south = prev_prop_west = prev_prop_east = 0
 
-        # RUST let mut prev_south = Row::default();
-        prev_prev_south = prev_prop_stay = prev_prop_south = prev_prop_west = prev_prop_east = 0
-
-        # RUST let mut curr = Proposal::default();
-        # RUST propose(
-        # RUST   &mut curr,
-        # RUST   self.data[1],
-        # RUST   self.data[0],
-        # RUST   self.data[2],
-        # RUST &self.proposal_sequence,
-        # RUST );
-        # RUST self.data[0] = curr.north;
-        # RUST let mut next;
-        # RUST for r_of_prev in 0..(self.height - 3)
-        # RUST   {
-        @board.each_cons(3).with_index do |rows, index_of_north_row|
-          # rows are north, current, south
-
-          # RUST     let r_of_current = r_of_prev + 1;
-          # RUST   let r_of_next = r_of_prev + 2;
-          # RUST   next = Proposal::default();
-          # RUST   propose(
-          # RUST     &mut next,
-          # RUST     self.data[r_of_next],
-          # RUST       self.data[r_of_next - 1],
-          # RUST       self.data[r_of_next + 1],
-          # RUST   &self.proposal_sequence,
-          # RUST   );
+        # In each iteration we propose moves for the middle row in the cons(3) window,
+        # but actualize moves for the top row in that window.
+        @board.each_cons(3).with_index do |rows, north_index|
+          # Propose moves in and from rows[1]
           curr_prop_stay, curr_prop_north, curr_prop_south, curr_prop_west, curr_prop_east = propose_row_moves(rows)
 
-          # RUST     let blocked = prev_south & next.north;
-          # RUST   resolve_block(blocked, &mut prev_south, &mut self.data[r_of_prev]);
-          # RUST   resolve_block(blocked, &mut next.north, &mut next.stay);
-          # RUST   let blocked = curr.less & curr.more;
-          # RUST   resolve_block_l(blocked, &mut curr.less, &mut curr.stay);
-          # RUST   resolve_block_m(blocked, &mut curr.more, &mut curr.stay);
-          if (blocked = prev_prev_south & curr_prop_north).positive?
-            prev_prev_south, @board[index_of_north_row - 1] = resolve_block(blocked, prev_prev_south, @board[index_of_north_row - 1])
-            curr_prop_north, curr_prop_stay                 = resolve_block(blocked, curr_prop_north, curr_prop_stay)
+          # Actualize proposed moves for rows[0]. These were proposed in the previous iteration.
+          # East-west collisions are detected and resolved in propose_row_moves().
+          # Detect and resolve north-south collisions.
+          if (blocked = two_prev_prop_south & curr_prop_north).positive?
+            two_prev_prop_south     &= ~blocked
+            curr_prop_north         &= ~blocked
+            @board[north_index - 1] |= blocked
+            curr_prop_stay          |= blocked
           end
 
-          # RUST   let arrived = prev_south | curr.less | curr.more | next.north;
-          arrived       = prev_prev_south | prev_prop_west | prev_prop_east | curr_prop_north
+          arrived       = curr_prop_north | two_prev_prop_south | prev_prop_west | prev_prop_east
+          any_elf_moved ||= arrived.positive?
 
-          # RUST   if !arrived.is_empty()
-          # RUST     {
-          # RUST       self.is_active = true;
-          # RUST     }
-          any_elf_moved = true if arrived.positive?
+          @board[north_index] = prev_prop_stay | arrived
 
-          # RUST     self.data[r_of_current] = curr.stay | arrived;
-          @board[index_of_north_row] = prev_prop_stay | arrived
-
-          # RUST     prev_south = curr.south;
-          prev_prev_south = prev_prop_south
-
-          # RUST     curr = next;
-          prev_prop_stay  = curr_prop_stay
-          prev_prop_south = curr_prop_south
-          prev_prop_west  = curr_prop_west
-          prev_prop_east  = curr_prop_east
-          # RUST     }
+          # Carry forward the proposal for row[1] into the next loop.
+          two_prev_prop_south = prev_prop_south
+          prev_prop_south     = curr_prop_south
+          prev_prop_west      = curr_prop_west
+          prev_prop_east      = curr_prop_east
+          prev_prop_stay      = curr_prop_stay
         end
-        # RUST     self.data[self.height - 2] |= prev_south;
-        @board[-2] |= prev_prev_south
-        any_elf_moved = true if prev_prev_south.positive?
 
-        # RUST     self.proposal_sequence.rotate_left(1);
+        @board[-2]    |= two_prev_prop_south
+        any_elf_moved ||= two_prev_prop_south.positive?
+
         @directions = @directions.rotate
 
         any_elf_moved
       end
 
+      NO_PROPOSAL = [0, 0, 0, 0, 0].freeze
+
       def propose_row_moves((north, current, south))
-        proposal = Array.new(5, 0)
         # proposal positions are these, in this order:
         # stay [0], north [1], south [2], west [3], east [4]
-        return proposal if current.zero?
+        return NO_PROPOSAL if current.zero?
 
-        west_shadow = current >> 1
-        east_shadow = current << 1
-        above3      = north | (north >> 1) | (north << 1)
-        below3      = south | (south >> 1) | (south << 1)
-        happy       = current & ~west_shadow & ~east_shadow & ~above3 & ~below3
-        unhappy     = current & ~happy
+        north_west = north << 1
+        north_east = north >> 1
+        south_west = south << 1
+        south_east = south >> 1
+        above3     = ~(north_west | north | north_east)
+        below3     = ~(south_west | south | south_east)
+        west       = ~(current << 1)
+        east       = ~(current >> 1)
+        happy      = current & above3 & below3 & west & east
+        unhappy    = current & ~happy
+        north_prop = south_prop = west_prop = east_prop = 0
         @directions.each do |direction|
           # noinspection RubyCaseWithoutElseBlockInspection
           case direction
             when :north
-              proposal[1] = unhappy & ~above3
-              unhappy     &= ~proposal[1]
+              north_prop = unhappy & above3
+              unhappy    &= ~north_prop
             when :south
-              proposal[2] = unhappy & ~below3
-              unhappy     &= ~proposal[2]
+              south_prop = unhappy & below3
+              unhappy    &= ~south_prop
             when :west # comparing to shadow in the opposite direction
-              from        = unhappy & ~west_shadow & ~(north >> 1) & ~(south >> 1)
-              proposal[3] = from << 1
-              unhappy     &= ~from
+              from      = unhappy & east & ~north_east & ~south_east
+              west_prop = from << 1
+              unhappy   &= ~from
             when :east
-              from        = unhappy & ~east_shadow & ~(north << 1) & ~(south << 1)
-              proposal[4] = from >> 1
-              unhappy     &= ~from
+              from      = unhappy & west & ~north_west & ~south_west
+              east_prop = from >> 1
+              unhappy   &= ~from
           end
+          break if unhappy.zero?
         end
-        if (east_west_blocked = proposal[3] & proposal[4]).positive?
-          proposal[3] &= ~east_west_blocked
-          proposal[4] &= ~east_west_blocked
-          unhappy     |= (east_west_blocked << 1) | (east_west_blocked >> 1)
+        if (east_west_blocked = west_prop & east_prop).positive?
+          west_prop &= ~east_west_blocked
+          east_prop &= ~east_west_blocked
+          unhappy   |= (east_west_blocked << 1) | (east_west_blocked >> 1)
         end
-        proposal[0] = happy | unhappy
-        proposal
-      end
-
-      def resolve_block(blocked, proposed, backup)
-        [proposed & ~blocked, backup | blocked]
+        [happy | unhappy, north_prop, south_prop, west_prop, east_prop]
       end
 
       def spread_out(max_rounds = 10)
         while propose_and_move
-          # puts "Round #{ @rounds }\n#{ render }\n"
           @rounds += 1
           break if @rounds > max_rounds
         end
-        # pp @board
         self
       end
 
       def count_spaces
         trim
-        @board.length * @board.map(&:bit_length).max - @board.map { _1.to_s(2).chars.map(&:to_i).sum }.sum
+        (@board.length * @board.map(&:bit_length).max) - @board.map { _1.to_s(2).chars.map(&:to_i).sum }.sum
       end
 
       def part_one
